8fde158a751f555a868f19a5ea01cdb3331ee23f
Eric Vizu <eric_vizu@hotmail.com>
2023-10-22 00:14:01 -0300

    Service layer, component registration

--- src/main/java/com/ericvizu/course/repositories/UserRepository.java
+++ src/main/java/com/ericvizu/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ericvizu.course.entities.User;
 
+// Opcional a anotation Repository pois UserRepository já está herdando do JpaRepository que está registrada como componente
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	
