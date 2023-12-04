972617255b2b95c75366add970a821c8c093900a
Eric Vizu <eric_vizu@hotmail.com>
2023-12-04 18:18:08 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/ericvizu/course/config/TestConfig.java
+++ src/main/java/com/ericvizu/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.ericvizu.course.entities.Category;
 import com.ericvizu.course.entities.Order;
+import com.ericvizu.course.entities.OrderItem;
 import com.ericvizu.course.entities.Product;
 import com.ericvizu.course.entities.User;
 import com.ericvizu.course.entities.enums.OrderStatus;
 import com.ericvizu.course.repositories.CategoryRepository;
+import com.ericvizu.course.repositories.OrderItemRepository;
 import com.ericvizu.course.repositories.OrderRepository;
 import com.ericvizu.course.repositories.ProductRepository;
 import com.ericvizu.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -73,6 +78,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 
--- src/main/java/com/ericvizu/course/repositories/OrderItemRepository.java
+++ src/main/java/com/ericvizu/course/repositories/OrderItemRepository.java
@@ -2,9 +2,9 @@ package com.ericvizu.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.ericvizu.course.entities.User;
+import com.ericvizu.course.entities.OrderItem;
 
 // Opcional a anotation Repository pois UserRepository já está herdando do JpaRepository que está registrada como componente
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
