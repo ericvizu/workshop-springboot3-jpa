cda178c077ccb917c28eff90037f0fa0ed1ec6db
Eric Vizu <eric_vizu@hotmail.com>
2023-12-04 19:13:42 -0300

    Subtotal & Total methods

--- src/main/java/com/ericvizu/course/entities/OrderItem.java
+++ src/main/java/com/ericvizu/course/entities/OrderItem.java
@@ -31,20 +31,24 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
